58e44ac9edd90606f22269f5e0e7333516089cc4
Maryam Fatima <maryam27cs@gmail.com>
2025-06-17 14:59:55 +0500

    Add files via upload

---
+++ src/assets/projectsData.js
@@ -0,0 +1,58 @@
+export const projectsData = [
+    {
+        projectName: 'Todo List',
+        projectDescription:
+            'Take your productivity to the next level with our full-stack Todo List app, expertly crafted with a robust backend and intuitive frontend! Built using cutting-edge technologies,  Whether you are a developer looking to showcase your skills or a user seeking a reliable task management solution, our full-stack Todo List app is the perfect showcase of modern web development!',
+        imageUrl: 'https://github.com/Maryamf27/imgs/blob/main/Screenshot%202025-01-18%20163952.png?raw=true',
+        projectUrl: 'https://todo-app-7343ea.netlify.app/',
+    },
+    {
+        projectName: 'Password Manager',
+        projectDescription:
+            'Take control of your digital security with our robust Password Manager, expertly crafted with a powerful backend and intuitive frontend using React! This cutting-edge application allows you to securely store, generate, and manage unique passwords for all your online accounts, ensuring your sensitive information remains protected.',
+        imageUrl: 'https://github.com/Maryamf27/imgs/blob/main/Screenshot%202025-01-18%20205851.png?raw=true',
+        projectUrl: 'https://password-manager-bdcaa4.netlify.app/',
+    },
+
+    {
+        projectName: 'Weather App',
+        projectDescription: `Our innovative weather app is designed to provide you with accurate and up-to-date weather information at your fingertips. With our user-friendly interface. Additionally, our app includes severe weather alerts, ensuring you stay informed and safe in case of extreme weather conditions.`,
+        imageUrl: 'https://github.com/Maryamf27/imgs/blob/main/Screenshot%202025-01-18%20164325.png?raw=true',
+        projectUrl: 'https://weather-52c712.netlify.app/',
+    },
+    {
+        projectName: 'Currency Converter',
+        projectDescription:
+            'Instantly convert currencies with ease using our intuitive and accurate currency converter! Designed to simplify international transactions, our converter provides up-to-date exchange rates for a vast array of global currencies. With a user-friendly interface and seamless functionality,.',
+        imageUrl: 'https://github.com/Maryamf27/imgs/blob/main/Screenshot%202025-01-18%20163245.png?raw=true',
+        projectUrl: 'https://converter-ce22fd.netlify.app/',
+    },
+    {
+        projectName: 'Spotify Clone',
+        projectDescription:
+            'Get ready to groove with our stunning Spotify clone, expertly crafted to deliver an unparalleled music streaming experience. This meticulously designed web application replicates the seamless user interface and intuitive functionality of the original Spotify platform, allowing users to effortlessly search, discover, and play their favorite songs, albums, and playlists.',
+        imageUrl: 'https://github.com/Maryamf27/imgs/blob/main/Screenshot%202025-01-18%20164247.png?raw=true',
+        projectUrl: 'https://spotify-clone-3beea6.netlify.app/',
+    },
+    {
+        projectName: 'Portfolio Clone',
+        projectDescription:
+            'Explore my digital footprint with my professionally designed portfolio, crafted with precision using HTML and CSS! This visually stunning showcase highlights my skills, experience, and accomplishments, providing a comprehensive overview of my expertise. ',
+        imageUrl: 'https://github.com/Maryamf27/imgs/blob/main/Screenshot%202025-01-18%20203455.png?raw=true',
+        projectUrl: 'https://portfolio-clone-6dc6df.netlify.app/'
+    },
+
+    // {
+    //     projectName: 'Portfolio Website',
+    //     projectDescription:
+    //         'As a developer, we should definitely have our own portfolio. There are endless benefits of it. so here is one of me! made it using react.js, html & css.',
+    //     imageUrl: 'https://github.com/Maryamf27/imgs/blob/main/Screenshot%202025-02-07%20141112.png?raw=true',
+    //     projectUrl: 'https://portfolio-8d3184.netlify.app/#/',
+    // },
+    {
+        projectName: 'Netflix',
+        projectDescription: `Immerse yourself in the ultimate streaming experience with our Netflix clone, expertly crafted using HTML and CSS. This sleek and responsive web application replicates the seamless user experience of the original Netflix platform, boasting a visually stunning interface that perfectly captures the essence of the iconic brand`,
+        imageUrl: 'https://github.com/Maryamf27/imgs/blob/main/netflix.png?raw=true',
+        projectUrl: 'https://netflix-project-woad.vercel.app/',
+    },
+];
---
+++ src/assets/skillsData.js
@@ -0,0 +1,42 @@
+export const skillList = [
+    {
+        skillName: 'HTML',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096301-84bb07bb-c807-46e4-a7d1-72e3f86c611c.png',
+    },
+    {
+        skillName: 'CSS',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096288-b6a601b7-7f5c-489e-be56-f9a7d61208f4.png',
+    },
+    {
+        skillName: 'JavaScript',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096309-ad1895af-57fc-4744-ad4c-35888c4f125b.png',
+    },
+    {
+        skillName: 'React',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096335-c6a18931-394d-444f-918a-51e1d052347b.png',
+    },
+    {
+        skillName: 'Redux',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096339-387cbc59-2630-458b-a2b7-a652245ea58a.png',
+    },
+    {
+        skillName: 'NodeJS',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096325-d548fb46-5806-41a1-8af6-43a1bdcc7f89.png',
+    },
+    {
+        skillName: 'Tailwind',
+        skillUrl: 'https://github.com/Maryamf27/imgs/blob/main/Tailwind%20CSS.png?raw=true',
+    },
+    {
+        skillName: 'ExpressJS',
+        skillUrl: 'https://github.com/Maryamf27/imgs/blob/main/Express.png?raw=true',
+    },
+    {
+        skillName: 'MongoDB',
+        skillUrl: 'https://user-images.githubusercontent.com/31516195/170096317-6eabc365-6fb1-41e2-b699-ea18f202a145.png',
+    },
+    {
+        skillName: 'Python',
+        skillUrl: 'https://github.com/Maryamf27/imgs/blob/main/Python.png?raw=true'
+    },
+];
